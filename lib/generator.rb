4958b510ae510e28e39ee355196d8daed6038db1
Michael Ries <michael@riesd.com>
2014-09-05 22:19:13 -0600

    a simplistic/naive approach to making trigrams work

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,5 +1,12 @@
 module Generator
   def self.generate(trigram, prompt, size)
-    ""
+    words = prompt.split " "
+    1.upto(size - 2).each do |i|
+      prompt = words[-2..-1].join(" ")
+      possibilities = trigram.get(prompt)
+      next unless possibilities
+      words << possibilities.sample
+    end
+    words.join " "
   end
 end
--- lib/trigram.rb
+++ lib/trigram.rb
@@ -1,11 +1,16 @@
 class Trigram
-  attr_reader :text
+  attr_reader :hash
 
   def initialize(text)
-    @text = text
+    @hash = {}
+    text.split(" ").each_cons(3) do |word_1, word_2, word_3|
+      key = "#{word_1} #{word_2}"
+      hash[key] ||= []
+      hash[key] << word_3
+    end
   end
 
   def get(two_words)
-
+    hash[two_words]
   end
 end
